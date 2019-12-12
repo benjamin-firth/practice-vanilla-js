4a2bfc6828a3251ebcea4ad756f6a954a6e8230d
Ben Firth <bfirth93@gmail.com>
2019-12-12 11:58:12 -0700

    Add delete functionality

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const climbs = [];
+let climbs = [];
 
 const addToDom = () => {
   $('#where-cards-go').html('');
@@ -11,7 +11,7 @@ const addToDom = () => {
           <h3>Grade: ${climb.grade}</h3>
           <p>Description:</p>
           <p>${climb.description}</p>
-          <button type="button" id="delete-button">SENT IT!</button>
+          <button type="button" id="${climb.id}">SENT IT!</button>
         </div>
       `)
     })
@@ -20,20 +20,32 @@ const addToDom = () => {
   giveClimbs();
 }
 
+
 const createObj = () => {
   let name = $('#climb-name').val();
   let grade = $('#climb-grade').val();
   let desc = $('#climb-description').val();
-
+  
   let climb = new Card(name, grade, desc);
   climbs.unshift(climb);
-
+  
   addToDom();
-
+  
   $('#climb-name').val('');
   $('#climb-grade').val('');
   $('#climb-description').val('');
 }
 
+const deleteCard = (e) => {
+  let cardID = e.target.id;
+  let newClimbs = climbs.filter(climb => {
+    console.log(climb.id, cardID);
+    return climb.id !== parseInt(cardID);
+  })
+  climbs = newClimbs;
+  console.log(climbs);
+  addToDom();
+}
 
 $('#input-info').click(createObj);
+$('#where-cards-go').click(deleteCard);
